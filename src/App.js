de80075a8dffea735c6dd8b8e3b3b010d8a92ea4
SidharthKRaj <sidharthkanakaraj@gmail.com>
2021-02-07 17:02:52 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import './App.css';
+import axios from 'axios'
+import { useState, useEffect } from 'react'
+import { Button } from '@material-ui/core'
+
+function App() {
+  
+  const [advice, setState] = useState('');
+
+  // useEffect(() => {
+  //   fetchAdvice();
+  // }, [])
+
+  const fetchAdvice = () => {
+    axios.get(`https://api.adviceslip.com/advice`)
+    .then((response) => {
+      setState(response.data.slip)
+      console.log(advice);
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+  }
+  
+  return (
+    <div className="App">
+      <div className="card">
+        <h1 className="heading">
+          {advice.advice}
+        </h1>
+        <Button variant='contained' color='primary' className='button' onClick={fetchAdvice}>
+          Fetch Advice
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
